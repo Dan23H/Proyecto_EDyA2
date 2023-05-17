318ea8796a7a5e4a27fcc29f75daa87d71d7aa11
Dan23H <123597766+Dan23H@users.noreply.github.com>
2023-05-17 16:18:08 -0500

    new file:   dibujantes/src/App.js 	renamed:    dibujantes/src/7T.png -> dibujantes/src/assets/7T.png 	new file:   dibujantes/src/components/access/LoginForm.jsx 	new file:   dibujantes/src/components/access/SignInForm.jsx 	modified:   dibujantes/src/components/home/Navbar.jsx

---
+++ dibujantes/src/components/access/SignInForm.jsx
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+import useSignInForm from "../../hooks/useSignInForm";
+import logo from "../../assets/7T.png";
+import styles from "./Logeo.module.css";
+import "../../bootstrap-css/bootstrap.min.css";
+
+export const SignInForm = () => {
+  const {
+    usuario,
+    correo,
+    contraseña,
+    cContraseña,
+    tomarUsuario,
+    tomarCorreo,
+    tomarContraseña,
+    tomarCContraseña,
+    handleRegistro,
+    errorContraseña,
+  } = useSignInForm();
+
+  return (
+    <>
+      <div className={styles.loginFormContainer}>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <form onSubmit={handleRegistro}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Nombre de usuario"
+            onChange={tomarUsuario}
+            value={usuario}
+            required
+          />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Correo electronico"
+            onChange={tomarCorreo}
+            value={correo}
+            required
+          />
+          <input
+            className={`${styles.input} ${
+              errorContraseña && styles.inputError
+            }`}
+            type="password"
+            placeholder="Contraseña"
+            onChange={tomarContraseña}
+            value={contraseña}
+            required
+          />
+          <input
+            className={`${styles.input} ${
+              errorContraseña && styles.inputError
+            }`}
+            type="password"
+            placeholder="Confirmar Contraseña"
+            onChange={tomarCContraseña}
+            value={cContraseña}
+            required
+          />
+          <div>
+            <button className={styles.signInButton} type="submit">
+              Registrarse
+            </button>
+            <Link to="/">
+              <button className={styles.signInButton} type="button">
+                Volver
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
