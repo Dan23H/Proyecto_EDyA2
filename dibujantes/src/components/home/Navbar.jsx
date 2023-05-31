69d784e078746413a0bdd6e0914bc3c701fbfba4
alexvaldes20202 <johan.valdes@uao.edu.co>
2023-05-30 20:06:24 -0500

    navbar y perfil

--- dibujantes/src/components/home/Navbar.jsx
+++ dibujantes/src/components/home/Navbar.jsx
@@ -24,7 +24,7 @@ export const Navbar = ({ searchBar, current, item1, item2, item3, item4 }) => {
       <div className="topboard">
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
           <Link to="/home">
-            <div className="logo-container">
+            <div className="logo-container" style={{position:"relative", left:"30px"}}>
               <img src={logo} alt="Logo" className="logo" />
               <div className="logo-hover">
                 <span className="hover-text">Ir al inicio</span>
@@ -44,22 +44,22 @@ export const Navbar = ({ searchBar, current, item1, item2, item3, item4 }) => {
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item" title={item1}>
+              <li className="nav-item" style={{position:"relative", left:"120px"}} title={item1}>
                 <Link className={fixCSS(item1, current)} to={fix(item1)}>
                   <img className="logo" src={subirImagen} alt={item1} />
                 </Link>
               </li>
-              <li className="nav-item" title={item2}>
+              <li className="nav-item" style={{position:"relative", left:"900px", top:"5px"}} title={item2}>
                 <Link className={fixCSS(item2, current)} to={fix(item2)}>
-                  <img className="logo" src={mensajeria} alt={item2} />
+                  <img className="logo" style={{width:"65px", height:"40px"}}src={mensajeria} alt={item2} />
                 </Link>
               </li>
-              <li className="nav-item" title={item3}>
+              <li className="nav-item" style={{position:"relative", left:"932px"}} title={item3}>
                 <Link className={fixCSS(item3, current)} to={{ pathname: '/perfil' }}>
-                  <img className="logo" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt={item3} />
+                  <img className="logo" style={{borderRadius:"100px", height:"50px", width:"50px"}} src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt={item3} />
                 </Link>
               </li>
-              <li className="nav-item" title={item4}>
+              <li className="nav-item" style={{position:"relative", left:"648px", top:"1px"}} title={item4}>
                 <Link className={fixCSS(item4, current)} to={fix(item4)}>
                   <img className="logo" src={notifications} alt={item4} />
                 </Link>
@@ -67,19 +67,13 @@ export const Navbar = ({ searchBar, current, item1, item2, item3, item4 }) => {
             </ul>
             {searchBar === true ? (
               <>
-                <form className="form-inline mt-2 mt-md-0">
-                  <input
+                <form className="form-inline mt-2 mt-md-0" style={{position:"relative", left:"-360px"}}>
+                  <input style={{borderRadius:"15px", padding:"8px 200px"}}
                     className="form-control mr-sm-2"
                     type="text"
-                    placeholder="Search"
+                    placeholder="Barra de busqueda"
                     aria-label="Search"
                   />
-                  <button
-                    className="btn btn-outline-success my-2 my-sm-0"
-                    type="submit"
-                  >
-                    Search
-                  </button>
                 </form>
               </>
             ) : (
--- dibujantes/src/pages/Image.jsx
+++ dibujantes/src/pages/Image.jsx
@@ -1,47 +1,80 @@
-import { Navbar, Album } from "../components"
-import { postsPrueba2 } from "../components/fake-data/album-data"
-import "../components/image/image.css"
+import { Navbar, Album } from "../components";
+import { postsPrueba2 } from "../components/fake-data/album-data";
+import "../components/image/image.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const data = {
-    msg: "Shinobu Oshino :3",
-    img: "https://c.wallhere.com/photos/7f/83/anime_Monogatari_Series_Oshino_Shinobu-125680.jpg!d",
-    category: "#Anime",
-}
+  msg: "Shinobu Oshino :3",
+  img: "https://c.wallhere.com/photos/7f/83/anime_Monogatari_Series_Oshino_Shinobu-125680.jpg!d",
+  category: "#Anime",
+};
 
 // Aquí se enviaría la información de la imagen a mostrar
 export const Image = () => {
-    return (
-        <div className="container"> 
-            <Navbar
-                searchBar={true}
-                current="interfaz-usuario"
-                item1="Subir Imagen"
-                item2="Mensajeria"
-                item3="Perfil"
-                item4="Notificaciones"
+  return (
+    <div className="container">
+      <Navbar
+        searchBar={true}
+        current="interfaz-usuario"
+        item1="Subir Imagen"
+        item2="Mensajeria"
+        item3="Perfil"
+        item4="Notificaciones"
+      />
+      <div className="image-container">
+        <img src={data.img} alt="" />
+      </div>
+      <div className="button-container">
+        <button
+          style={{
+            borderRadius: "10px",
+            fontSize: "30px",
+            padding: "0px 10px",
+          }}
+        >
+          👍
+        </button>
+        <button
+          style={{
+            borderRadius: "10px",
+            fontSize: "30px",
+            padding: "0px 10px",
+          }}
+        >
+          👎
+        </button>
+        <button style={{ borderRadius: "10px" }}>Up</button>
+        <button style={{ borderRadius: "10px" }}>Down</button>
+        <button style={{ borderRadius: "10px" }}>Subscribe</button>
+        <button style={{ borderRadius: "10px" }}>Report</button>
+        <button style={{ borderRadius: "10px" }}>Download</button>
+      </div>
+      <div className="message-container">
+        <div>{data.msg}</div>
+        <div>
+          <h2>Comentarios</h2>
+          <ul>
+            {comments.map((comment) => (
+              <li key={comment._id}>
+                <strong>{comment.author}</strong>: {comment.text}
+              </li>
+            ))}
+          </ul>
+          <form onSubmit={handleFormSubmit}>
+            <input
+              type="text"
+              placeholder="Escribe un comentario..."
+              value={newComment}
+              onChange={handleInputChange}
             />
-            <div className="image-container">
-                <img src={data.img} alt="" />
-            </div>
-            <div className="button-container">
-                <button>Up</button>
-                <button>Down</button>
-                <button>Subscribe</button>
-                <button>Report</button>
-                <button>Download</button>
-            </div>
-            <div className="message-container">
-                <div>
-                    {data.msg}
-                </div>
-                <div>
-                    Comentarios
-                </div>
-            </div>
-            <div className="album">
-            <Album posts={postsPrueba2} category={'#Futuristic'} />
-            </div>
+            <button type="submit">Enviar</button>
+          </form>
         </div>
-
-    )
-}
+      </div>
+      <div className="album">
+        <Album posts={postsPrueba2} category={"#Futuristic"} />
+      </div>
+    </div>
+  );
+};
